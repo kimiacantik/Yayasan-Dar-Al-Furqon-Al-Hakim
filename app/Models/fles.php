9b5e0ff26018cc63bc0f9286887fa7c9be68d941
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-07 22:43:56 +0700

    Update fles.php
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- app/Models/fles.php
+++ app/Models/fles.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Scout\Searchable;
 
 class fles extends Model
@@ -31,24 +30,26 @@ class fles extends Model
         'published' => 'boolean',
     ];
 
-    public function scopePublished(Builder $query)
-    {
-        return $query->where('published', 1);
-    }
+public function getRouteKeyName()
+ {
+return 'slug';
+}
 
-    public function users(): BelongsTo
+    public function scopePublished($query)
     {
-        return $this->belongsTo(User::class);
+        return $query->where('published', true);
     }
 
     public function toSearchableArray(): array
     {
-        'file_id'=>$this->file_id,
+       return [
+ 'file_id'=>$this->file_id,
         'nama_file'=>$this->nama_file,
         'deskripsi'=>$this->deskripsi,
         'gambar'=>$this->gambar,
         'nama_pembuat'=>$this->nama_pembuat,
         'published'=>$this->published,
         'id'=>$this->id
+];
     }
 }
